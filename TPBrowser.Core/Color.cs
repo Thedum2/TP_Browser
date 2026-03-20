a32d9a3df01c2d7f227744e84d4a12f39e40effe
Thedum2 <asdf74591@gmail.com>
2026-03-20 16:05:58 +0900

    feat: [TGIENE-012~19] Core 구조 및 기본 데이터 타입 추가

---
+++ TPBrowser.Core/Color.cs
@@ -0,0 +1,36 @@
+﻿namespace TPBrowser.Core;
+
+public readonly struct Color
+{
+    public byte R { get; }
+    public byte G { get; }
+    public byte B { get; }
+    public byte A { get; }
+
+    public Color(byte r, byte g, byte b, byte a = 255)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+    }
+
+    public Color(string hex)
+    {
+        hex = hex.TrimStart('#');
+
+        int r = Convert.ToInt32(hex.Substring(0, 2), 16);
+        int g = Convert.ToInt32(hex.Substring(2, 2), 16);
+        int b = Convert.ToInt32(hex.Substring(4, 2), 16);
+        int a = hex.Length >= 8 ? Convert.ToInt32(hex.Substring(6, 2), 16) : 255;
+
+        R = (byte)r;
+        G = (byte)g;
+        B = (byte)b;
+        A = (byte)a;
+    }
+
+    public static readonly Color Transparent = new(0, 0, 0, 0);
+    public static readonly Color Black = new(0, 0, 0, 255);
+    public static readonly Color White = new(255, 255, 255, 255);
+}
---
+++ TPBrowser.Core/Size.cs
@@ -0,0 +1,16 @@
+﻿namespace TPBrowser.Core;
+
+public readonly struct Size
+{
+    // 객체의 크기 표현용
+    
+    public int Width { get; }
+
+    public int Height { get; }
+
+    public Size(int width, int height)
+    {
+        Width = width < 0 ? 0 : width;
+        Height = height < 0 ? 0 : height;
+    }
+}
